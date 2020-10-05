a7ad637aeb9e3ca15abf9aa8f9319d83089666cf
Nicholas Ingolia <nick@ingolia.org>
2020-10-05 11:16:16 -0700

    Include oligo sequence

--- barcode_v1_181005/assignment/targeting.R
+++ barcode_v1_181005/assignment/targeting.R
@@ -1,7 +1,7 @@
 options(stringsAsFactors=FALSE)
 
 barcodeGuides <- read.delim("../grna-assign-barcode-grna-good.txt")
-guideTargets <- read.delim("../../library_v1/guide_choice/guide-good-targets.txt")
+guideTargets <- read.delim("../../library_v1/sequence-good-targets.txt")
 
 barcodeTargets <- cbind.data.frame(barcodeGuides,
                                    guideTargets[match(barcodeGuides$guide, guideTargets$Guide),])
@@ -10,7 +10,7 @@ barcodeTargets <- barcodeTargets[!is.na(barcodeTargets$Guide),]
 barcodeTargets <- barcodeTargets[,c("barcode", "PrimaryGuide", "TargetLoc",
                                     "Yorf1", "Offset1", "StartType1",
                                     "Yorf2", "Offset2", "StartType2",
-                                    "Yorfs")]
+                                    "Yorfs", "Oligo")]
 barcodeTargets <- barcodeTargets[!is.na(barcodeTargets$Yorf1),]
 barcodeTargets <- barcodeTargets[order(barcodeTargets$PrimaryGuide),]
 write.csv(barcodeTargets, "../barcode-targets-good.txt", row.names=FALSE)
