c5726ee983d7368ae348e7c0b252d120a923f732
Nicholas Ingolia <nick@ingolia.org>
2021-02-14 22:48:38 -0800

    GO analysis of fitness scores

---
+++ mcglincy_2019/phenotype/yorf_go.R
@@ -0,0 +1,25 @@
+options(stringsAsFactors=FALSE)
+
+workdir <- "work" ## Sys.getenv("WORKDIR")
+
+yorfs <- read.csv(sprintf("%s/nizm005-yorf-deseq.csv", workdir))
+
+write.table(yorfs[grepl("^Y", yorfs$X), c("X", "lfcMin"),],
+            file=sprintf("%s/yorf-lfcmin-all.txt", workdir),
+            quote=FALSE, row.names=FALSE, col.names=FALSE, sep="\t")
+
+write.table(yorfs[grepl("^Y", yorfs$X) & yorfs$essential, c("X", "lfcMin"),],
+            file=sprintf("%s/yorf-lfcmin-ess.txt", workdir),
+            quote=FALSE, row.names=FALSE, col.names=FALSE, sep="\t")
+
+write.table(yorfs[grepl("^Y", yorfs$X) & yorfs$essential & yorfs$clean, c("X", "lfcMin"),],
+            file=sprintf("%s/yorf-lfcmin-ess-clean.txt", workdir),
+            quote=FALSE, row.names=FALSE, col.names=FALSE, sep="\t")
+
+write.table(yorfs[grepl("^Y", yorfs$X) & !yorfs$essential, c("X", "lfcMin"),],
+            file=sprintf("%s/yorf-lfcmin-via.txt", workdir),
+            quote=FALSE, row.names=FALSE, col.names=FALSE, sep="\t")
+
+write.table(yorfs[grepl("^Y", yorfs$X) & !yorfs$essential & yorfs$clean, c("X", "lfcMin"),],
+            file=sprintf("%s/yorf-lfcmin-via-clean.txt", workdir),
+            quote=FALSE, row.names=FALSE, col.names=FALSE, sep="\t")
